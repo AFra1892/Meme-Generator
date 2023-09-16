0c83611b8b78bb79071610a1cbb7ccf99d691236
amir <mashhadifarahani1@gmail.com>
2023-09-16 21:47:01 +0330

    header components

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <h3>Meme Generator</h3>
+            <p>React Course-Project 3</p>
+        </header>
+    )
+}
